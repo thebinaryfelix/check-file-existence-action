5993240a155b4e528038d07b0bf9f9c349fcbe4e
Mateus Félix <mateus.cfoliveira@gmail.com>
2022-10-22 20:19:45 -0300

    chore: add checkout step

--- index.js
+++ index.js
@@ -4,12 +4,12 @@ const github = require("@actions/github");
 try {
   const files = core.getInput("files");
 
-  console.log(`Hello ${files}!`);
+  console.log(`Files: ${files}!`);
 
-  // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(github.context.payload, undefined, 2);
+  // // Get the JSON webhook payload for the event that triggered the workflow
+  // const payload = JSON.stringify(github.context.payload, undefined, 2);
 
-  console.log(`The event payload: ${payload}`);
+  // console.log(`The event payload: ${payload}`);
 } catch (error) {
   core.setFailed(error.message);
 }
