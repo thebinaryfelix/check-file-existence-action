302d757e13400d67461c1ca1c1e3cd178bed0957
Mateus Félix <mateus.cfoliveira@gmail.com>
2022-10-22 21:15:26 -0300

    chore: use glob from actions

--- index.js
+++ index.js
@@ -1,15 +1,61 @@
 const core = require("@actions/core");
-const github = require("@actions/github");
+// const github = require("@actions/github");
+const glob = require("@actions/glob");
 
-try {
-  const files = core.getInput("files");
+const run = async () => {
+  try {
+    const missingFiles = [];
 
-  console.log(`Files: ${files}!`);
+    const fileNamesInput = core.getInput("files");
 
-  // // Get the JSON webhook payload for the event that triggered the workflow
-  // const payload = JSON.stringify(github.context.payload, undefined, 2);
+    const files = fileNamesInput.split(",").map((file) => file.trim());
 
-  // console.log(`The event payload: ${payload}`);
-} catch (error) {
-  core.setFailed(error.message);
-}
+    const globOptions = {
+      followSymbolicLinks: false,
+      // core.getInput("follow-symbolic-links").toUpper() !== "FALSE",
+    };
+
+    const globber = await glob.create(files.join("\n"), globOptions);
+
+    for await (const file of globber.globGenerator()) {
+      console.log(file);
+    }
+
+    console.log(`Files to look for: ${fileNamesInput}`);
+
+    console.log(`Files: ${files}`);
+
+    // await Promise.all(
+    //   files.map(async (file) => {
+    //     const isPresent = await checkExistence(file);
+
+    //     if (!isPresent) {
+    //       missingFiles.push(file);
+    //     }
+    //   })
+    // );
+
+    // if (missingFiles.length > 0) {
+    //   console.log("Missing files:", missingFiles);
+    // }
+
+    // await Promise.all(
+    //   fileList.map(async (file: string) => {
+    //     const isPresent = await checkExistence(file)
+    //     if (!isPresent) {
+    //       missingFiles.push(file)
+    //     }
+    //   })
+    // )
+
+    // // Get the JSON webhook payload for the event that triggered the workflow
+    // const payload = JSON.stringify(github.context.payload, undefined, 2);
+
+    // console.log(`The event payload: ${payload}`);
+  } catch (error) {
+    // core.setFailed(error.message);
+    console.log("FAILED:", error);
+  }
+};
+
+run();
